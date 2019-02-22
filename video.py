22155675d7cb7c0164fc29e0eaf9875f61b21aac
philipok-1 <philipworman@gmail.com>
2019-02-22 12:53:16 +0000

    push to record function

--- video.py
+++ video.py
@@ -1,41 +1,51 @@
 import numpy as np
-import time
+import time, datetime
 import cv2
 from gpiozero import Button, LED
 
 button = Button(22)
 led=LED(17)
 
-cap = cv2.VideoCapture(0)
-
 #filming time constants
 
 COUNT=0
 TIMER=25
 
+#camera mode
+
+camera_mode="idle"
+
+#check mode
+
+def film(COUNT=COUNT, TIMER=TIMER):
+    
+    global camera_mode
+
+    cap = cv2.VideoCapture(0)
+
 #measure script execution time
 
-time_elapsed=time.time()
+    time_elapsed=time.time()
 
 #set up video output
 
-timestr = time.strftime("%Y%m%d-%H%M%S")
+    timestr = time.strftime("%Y%m%d-%H%M%S")
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter(timestr+'.avi',fourcc, 20.0, (640,480))
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(timestr+'.avi',fourcc, 20.0, (640,480))
 
 #screen text (for debugging)
 
-font                   = cv2.FONT_HERSHEY_SIMPLEX
-bottomLeftCornerOfText = (10,250)
-fontScale              = 1
-fontColor              = (255,255,255)
-lineType               = 2
+    font                   = cv2.FONT_HERSHEY_SIMPLEX
+    bottomLeftCornerOfText = (10,250)
+    fontScale              = 1
+    fontColor              = (255,255,255)
+    lineType               = 2
 
-while(True):
+    while(True):
 
     # Capture frame-by-frame
-    ret, frame = cap.read()
+        ret, frame = cap.read()
 
     # write text to frame
 #    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -47,31 +57,60 @@ while(True):
     #out.write(frame)
 
     # Display frame
-    cv2.imshow('frame', frame)
+        cv2.imshow('frame', frame)
 
     #increment count
 
-    COUNT+=1
-    flash=COUNT
+        COUNT+=1
+        flash=COUNT
 
     #flash LED every second
-    if flash%10==0:
-       led.toggle()
-
+        if flash%10==0:
+           led.toggle()
 
     #exit 
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    if COUNT==(TIMER*10):
-        break
+        if COUNT==(TIMER*10):
+            break
+
+        if button.is_pressed==False:
+
+            camera_mode=False
+            break
 
 # done - capture release
 
-total_time=time.time()-time_elapsed
-print ("total time="+str(total_time))
-print ("frames/second="+str(COUNT/total_time))
+    total_time=time.time()-time_elapsed
+    print ("total time="+str(total_time))
+    print ("frames/second="+str(COUNT/total_time))
+    led.off()
+    cap.release()
+    cv2.destroyAllWindows()
 
-cap.release()
-cv2.destroyAllWindows()
+    return
+
+#main script
+
+print ("ready")
+
+while True:
+
+    if button.is_pressed:
+
+       if button.is_held:
+
+           camera_mode="active"
+           button_count=0
+
+    else: camera_mode="idle"
+
+    if camera_mode=="active":
+
+        film()
+
+    
+
+    
