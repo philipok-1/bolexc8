a3b5825419d8878309445a7ec2c2e448b2b1eea0
philipok-1 <philipworman@gmail.com>
2019-03-01 13:36:12 +0000

    test conversion script using subprocess ffmpeg

---
+++ convert.py
@@ -0,0 +1,16 @@
+
+import subprocess
+import time
+
+
+
+def convert_video(input_file):
+
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    filename=str(input_file)
+    subprocess.call(['ffmpeg', '-r','20', '-i', filename, '-b:a', '128k', '-c:v', 'libx264','-crf', '23',  timestr+".mp4"], shell=False)    
+    print ("conversion complete")
+    return 
+
+convert_video("20190301-103042.avi")
+
